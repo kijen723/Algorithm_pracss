016261ede90163a20ed37040fcc172c2671d5b35
kijen723 <kij2646@gmail.com>
2024-01-06 18:03:01 +0900

    add swea 1204.py

---
+++ swea/1204/1204.py
@@ -0,0 +1,21 @@
+# swea 1204.py
+
+T = int(input())
+
+for test_case in range(1, T + 1):
+    N = int(input())
+    scores = list(map(int, input().split()))
+
+    dictionary_of_scores = {}
+    
+    for score in scores:
+        if score in dictionary_of_scores:
+            dictionary_of_scores[score] += 1
+        else:
+            dictionary_of_scores[score] = 1
+    
+    sorted_dictionary = sorted(dictionary_of_scores.items(), key = lambda item: (item[1], item[0]), reverse=True)
+    
+    answer = sorted_dictionary[0][0]
+    
+    print(f"#{N} {answer}")
