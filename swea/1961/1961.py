ccecf460ee63d13e4f4f6badcf4743593d31d787
kijen723 <kij2646@gmail.com>
2024-01-08 20:36:29 +0900

    add swea 1961.py

---
+++ swea/1961/1961.py
@@ -0,0 +1,24 @@
+# swea 1964.py
+
+T = int(input())
+
+for test_case in range(1, T + 1):
+    print(f"#{test_case}")
+    
+    N = int(input())
+    
+    matrix = []
+    answer = [[], [], []]
+    
+    for i in range(N):
+        matrix.append(list(map(str, input().split())))
+    
+    for i in range(N):
+        temp1, temp2, temp3 = "", "", ""
+        
+        for j in range(N):
+            temp1 += matrix[N - 1 - j][i]
+            temp2 += matrix[N - 1 - i][N - 1 - j]
+            temp3 += matrix[j][N - 1 - i]
+        
+        print(f"{temp1} {temp2} {temp3}")
