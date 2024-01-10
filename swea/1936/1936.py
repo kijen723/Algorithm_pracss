9158202b8cab1fb996995a556dbd547baf2effa5
kijen723 <kij2646@gmail.com>
2024-01-10 19:19:29 +0900

    add swea 1936.py

---
+++ swea/1936/1936.py
@@ -0,0 +1,14 @@
+# swea 1936.py
+
+A, B = map(int, input().split())
+
+if A > B:
+    if A == 3 and B == 1:
+        print("B")
+    else:
+        print("A")
+else:
+    if B == 3 and A == 1:
+        print("A")
+    else:
+        print("B")
